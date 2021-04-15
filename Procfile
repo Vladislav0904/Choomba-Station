09384599efc4f5f04223b10428243514f766cefa
BADBOYKILLA <33057075+vladislav0904@users.noreply.github.com>
2021-04-15 20:14:41 +0500

    First version of Choomba Station

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python choomba_main.py
---
+++ choomba_main.py
@@ -0,0 +1,79 @@
+from flask import Flask, request
+import logging
+import json
+import random
+
+app = Flask(__name__)
+
+sessionStorage = {}
+
+logging.basicConfig(level=logging.INFO)
+
+
+@app.route('/post', methods=['POST'])
+def main():
+    logging.info(f'Request: {request.json!r}')
+    response = {
+        'session': request.json['session'],
+        'version': request.json['version'],
+        'response': {
+            'end_session': False
+        }
+    }
+    handle_dialog(response, request.json)
+    logging.info(f'Response: {response!r}')
+    return json.dumps(response)
+
+
+def handle_dialog(res, req):
+    user_id = req['session']['user_id']
+
+    # если пользователь новый, то просим его представиться.
+    if req['session']['new']:
+        res['response']['text'] = 'Привет, чумба!'
+        # создаем словарь в который в будущем положим имя пользователя
+        sessionStorage[user_id] = {
+            'first_name': None
+        }
+        return
+
+    if sessionStorage[user_id]['first_name'] is None:
+        # в последнем его сообщение ищем имя.
+        first_name = get_first_name(req)
+        # если не нашли, то сообщаем пользователю что не расслышали.
+        if first_name is None:
+            res['response']['text'] = \
+                'Не расслышала имя. Повтори, пожалуйста!'
+        # если нашли, то приветствуем пользователя.
+        # И спрашиваем какой город он хочет увидеть.
+        else:
+            sessionStorage[user_id]['first_name'] = first_name
+            res['response'][
+                'text'] = 'Приятно познакомиться, ' \
+                          + first_name.title() \
+                          + '. Я - Скиппи.'
+    else:
+        if 'включи что-нибудь' in req['request']['original_utterance'].lower():
+            res['response'][
+                'tts'] = '<speaker audio="dialogs-upload/f0b2392a-f08b-404f-af1a-c0109eab8a69/f02c9bc2-5a31-426b-919b' \
+                         '-695d4058793a.opus"> '
+            res['response']['card']['type'] = 'BigImage'
+            res['response']['text'] = 'Cкрашиваю ваше ожидание приятной музыкой.'
+        else:
+            res['response']['text'] = \
+                'Чумба, ты тупой'
+
+
+def get_first_name(req):
+    # перебираем сущности
+    for entity in req['request']['nlu']['entities']:
+        # находим сущность с типом 'YANDEX.FIO'
+        if entity['type'] == 'YANDEX.FIO':
+            # Если есть сущность с ключом 'first_name',
+            # то возвращаем ее значение.
+            # Во всех остальных случаях возвращаем None.
+            return entity['value'].get('first_name', None)
+
+
+if __name__ == '__main__':
+    app.run()
